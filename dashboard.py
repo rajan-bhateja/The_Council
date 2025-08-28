79e0114f14bf35fdaf3e873f9e66c3bb4778f15a
rajan <rajanbhateja6@gmail.com>
2025-08-28 17:46:03 +0530

    Fixed individual tab's content

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from agents import workflow
+import json
 
 st.title("The Council")
 st.caption("Powered by OpenAI and Swarms")
@@ -11,28 +12,35 @@ home_tab, overview_tab, pros_cons_tab, practical_tab, expert_tab = st.tabs(["Hom
 with home_tab:
     with st.form(key="my_form"):
         chat = ""
-        query = st.text_input("Enter a query:", placeholder="How do I build a house from cheese?")
+        query = st.text_input("Enter a query:", placeholder="How do I build a cheese house?")
         submitted = st.form_submit_button("Submit")
 
     if query!="":
         chat = workflow.run(query)
+        with open("response.txt", "w") as file:
+            json.dump(chat, file, ensure_ascii=True)
+
         with overview_tab:
             st.header("Overview")
             with st.spinner():
-                if chat!="":
-                    st.markdown(chat)
-                    print(chat)
+                st.markdown(chat[2]["content"])
 
         with pros_cons_tab:
             st.header("Pros & Cons")
-            st.markdown(chat)
+            with st.spinner():
+                st.markdown(chat[3]["content"])
 
         with practical_tab:
             st.header("Practical")
-            st.markdown(chat)
+            with st.spinner():
+                st.markdown(chat[4]["content"])
 
         with expert_tab:
             st.header("Expert")
-            st.markdown(chat)
+            with st.spinner():
+                st.markdown(chat[5]["content"])
+
+        if chat:
+            st.success("Response generated!")
     else:
         st.info("Please submit a query to continue")
